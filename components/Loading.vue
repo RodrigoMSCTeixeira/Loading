07614691f4f2f4639e607a4613ad883ff0933cd1
RodrigoMSCTeixeira <rodrigoteixeira@sefaz.pi.gov.br>
2022-06-28 13:21:06 -0300

    first commit

---
+++ components/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="loading" class="loading-page">
+    <div class="loading"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+
+  methods: {
+    start() {
+      this.loading = true
+    },
+    finish() {
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style>
+@import '~/static/css/loading.css';
+</style>
